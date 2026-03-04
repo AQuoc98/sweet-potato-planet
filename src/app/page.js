6bbd09e385987de94ba6f096c788c09a8b151165
AQuoc98 <quocnguyen.developer@gmail.com>
2026-03-04 10:43:06 +0700

    feat(1.0): Add hero section UI

--- src/app/page.js
+++ src/app/page.js
@@ -1,3 +1,19 @@
 export default function Home() {
-  return <div className="text-orange-300">This my home page</div>;
+  return (
+    <>
+      {/* Hero Section */}
+      <section className="bg-[url('/hero.jpg')] bg-cover bg-no-repeat bg-center">
+        <div className="flex flex-row flex-nowrap justify-center items-center h-125">
+          <div className="flex flex-col items-center gap-4 px-4">
+            <h1 className="p-4 mb-4 rounded font-bold text-white bg-red-400 text-4xl md:text-7xl shadow-[-1px_-5px_20px_0px_rgba(0,0,0,0.3)] animate-[rotateZoom_1s_ease-out_both]">
+              Beautiful Scrunchie
+            </h1>
+            <h2 className="p-4 rounded text-white bg-yellow-400 text-2xl md:text-5xl font-bold shadow-[-1px_-5px_20px_0px_rgba(0,0,0,0.3)] animate-[animate_1s_ease-out_0.5s_both]">
+              from the best tailor
+            </h2>
+          </div>
+        </div>
+      </section>
+    </>
+  );
 }
